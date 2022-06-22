e8aafc5f0cc98c092c879cf6f232d51e894c192f
Olga Panfilova <gr-olga@ya.ru>
2022-06-22 14:56:22 +0200

    add router for space

--- routers/space.js
+++ routers/space.js
@@ -6,8 +6,24 @@ const router = new Router()
 
 router.get("/", async (req, res) => {
     try {
-        const products = await Space.findAll({ include: Story });
-        res.send(products);
+        const spaces = await Space.findAll({ include: Story });
+        res.send(spaces);
+    } catch (e) {
+        console.log(e.message);
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const productId = req.params.id;
+
+        const oneSpace = await Space.findByPk(productId, { include: Story });
+
+        if (!oneSpace) {
+            return res.status(404).send("User not found");
+        }
+
+        res.send(oneSpace);
     } catch (e) {
         console.log(e.message);
     }
