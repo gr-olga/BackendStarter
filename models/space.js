943a97d2484d2fb1e42f0eee614a24f201d5c552
Olga Panfilova <gr-olga@ya.ru>
2022-06-22 12:43:40 +0200

    first commit. create spaces and relation

---
+++ models/space.js
@@ -0,0 +1,22 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class space extends Model {
+    static associate(models) {
+      space.belongsTo(models.user)
+    }
+  }
+
+  space.init({
+    title: {type: DataTypes.STRING, allowNull: false},
+    description: DataTypes.TEXT,
+    backgroundColor: DataTypes.STRING,
+    color: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'space',
+  });
+  return space;
+};
--- models/user.js
+++ models/user.js
@@ -1,37 +1,46 @@
 "use strict";
 
-const { Model } = require("sequelize");
+const {Model} = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class user extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class user extends Model {
+        static associate(models) {
+            user.hasOne(models.space)
+        }
     }
-  }
-  user.init(
-    {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    },
-    {
-      sequelize,
-      modelName: "user",
-    }
-  );
-  return user;
+
+    user.init(
+        {
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: DataTypes.STRING,
+                unique: true,
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            sequelize,
+            modelName: "user",
+        }
+    );
+    return user;
 };
+
+
+//npx sequelize-cli model:generate --name space --attributes title:string,description:text,backgroundColor:string,color:string
+
+
+//id              | Integer   | yes      | Already added by model:generate                                     |
+// | title           | String    | yes      |                                                                     |
+// | description     | Text      | no       |                                                                     |
+// | backgroundColor | String    | no       | This will always be a hexcode and default should be #ffffff (white) |
+// | color           | String    | no       | This will always be a hexcode and default should be #000000 (black) |
+// | createdAt       | Date      | yes      | Already added by model:generate                                     |
+// | updatedAt       | Date      | yes      | Already added by model:generate                                     |
+// | userId          | Integer   | yes      | Foreign key (references a user)                                     |
